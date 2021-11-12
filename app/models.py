060e8940014cdd63a1311444612e66b331f1a44c
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-13 01:25:02 +0300

    db

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
+from . import db
+
 #quote model 
 class Quotes: 
   '''Class to display random quotes'''
   def __init__(self,author,quote):
     self.author = author
     self.quote = quote
+# db model
+    
