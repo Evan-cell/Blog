8b974797326a10c8e714fa4b9d9684164f6cfd44
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-12 18:24:02 +0300

    views

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,14 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-# from ..requests import get_movies,get_movie,search_movie
-# from .forms import ReviewForm
-# from ..models import Revie
+from ..requests import get_quotes
+
 # Views
-@main.route('/')
-def index():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-
-    message = 'Hello World'
-    return render_template('index.html',message = message)
+@main.route('/',methods = ["GET"])
+def  index():
+  '''
+  Function that returns the home page
+  '''
+#   blogs_found = Blogs.query.order_by(Blogs.submitted.desc()).all()
+  quotes = get_quotes()
+  title = "kim blog"
+  return render_template('index.html',title = title,quotes = quotes)
