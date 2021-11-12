75fd1601c7141c7557b617bf59f91117cad5dc1f
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-13 02:17:34 +0300

    pass secure

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ from flask_script import Manager,Server
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 @manager.shell
 def make_shell_context():
