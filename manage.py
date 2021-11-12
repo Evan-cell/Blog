fff93f5283d7a883f843aea9601041c87705b298
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-13 02:11:34 +0300

    migration

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager,Server
 
 # Creating app instance
@@ -7,5 +8,8 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User )
 if __name__ == '__main__':
     manager.run()
