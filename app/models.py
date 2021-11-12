38964308e4157a85495ff2b6edff5556cdd620dd
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-13 01:26:05 +0300

    users

--- app/models.py
+++ app/models.py
@@ -7,4 +7,10 @@ class Quotes:
     self.author = author
     self.quote = quote
 # db model
-    
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'    
