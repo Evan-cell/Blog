a39b4e8c532590db955979135970e5014fd22cf5
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-13 09:04:28 +0300

    login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -19,6 +21,8 @@ def create_app(config_name):
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     # Will add the views and forms
 
