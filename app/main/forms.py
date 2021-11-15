debeef0121428101bb26152253a37089cfb6390c
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-11-15 08:27:48 +0300

    profile

---
+++ app/main/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField,TextAreaField
+from wtforms.validators import Required,Email,EqualTo
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
+from .forms import UpdateProfile
+from .. import db
 from ..requests import get_quotes
 from flask_login import login_required
+from ..models import  User
 
 # Views
 @main.route('/',methods = ["GET"])
@@ -13,3 +16,28 @@ def  index():
   quotes = get_quotes()
   title = "kim blog"
   return render_template('index.html',title = title,quotes = quotes)
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)  
